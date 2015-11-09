b05ba0d0037643bab1fcaac2b817797cba6f9d76
christinakim <christinahkim1@gmail.com>
2015-11-09 15:08:09 -0600

    changing it to any port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/views'); // general config
 app.set('view engine', 'jade');
 
 
-var server = app.listen(5000, function () {
+var server = app.listen(process.env.PORT ||5000, function () {
 	var host = server.address().address;
   	var port = server.address().port;
 
