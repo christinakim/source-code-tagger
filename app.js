23ca492b68111bbf1441dfec572c6de30157eb66
christinakim <christinahkim1@gmail.com>
2015-11-09 01:37:31 -0600

    adding request and cheerio, hardcoded URL to return all nodes in DOM

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
+var request = require("request");
+var cheerio = require("cheerio");
 var express = require('express');
 var app = express();
+
+app.get('/', function (req, res) {
+	var url = "http://www.christinakim.me/";
+    request(url, function(error, response, html){
+        if(!error){
+        	var $ = cheerio.load(html),
+            	//returns all elements between html tags as nodes  
+            	data = $("html *");
+        }
+        console.log(data);
+    })
+
+});
 
 var server = app.listen(5000, function () {
 	var host = server.address().address;
