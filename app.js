a79d2ee912f0c8cb92df986102baf82ef4bac104
christinakim <christinahkim1@gmail.com>
2015-11-09 02:02:22 -0600

    obj returns tags and count of tags

--- app.js
+++ app.js
@@ -7,11 +7,30 @@ app.get('/', function (req, res) {
 	var url = "http://www.christinakim.me/";
     request(url, function(error, response, html){
         if(!error){
-        	var $ = cheerio.load(html),
+        	var name, 
+        		//obj where key = tag, val = tag count
+        		tags = {},
+        		$ = cheerio.load(html),
             	//returns all elements between html tags as nodes  
             	data = $("html *");
+
+			//traverse DOM to get name of each node/element
+            for (i = 0; i < data.length; i++) { 
+    			tag = data.get(i).name;
+
+    			//if it's the first time we're seeing the tag add it to tags and set the count to 1
+    			if(tags[tag] === undefined){
+    				tags[tag] = 1;
+    			} else {
+    				//otherwise add 1 
+    				tags[tag] = tags[tag] + 1;
+    			}
+			}
+
         }
-        console.log(data);
+        console.log(data.length);
+
+        res.send(tags);
     })
 
 });
